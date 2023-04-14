2ee7a717285ba769a4d405191fdfb725f5fae39e
lawkelvin33 <lawkelvin33@korea.ac.kr>
2023-04-15 01:38:34 +0900

    validation function, createPosition controller, createPosition function interface

--- src/api/V2/defutures/defutures.controller.ts
+++ src/api/V2/defutures/defutures.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  Body,
+  ParseIntPipe,
+} from "@nestjs/common";
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from "@nestjs/swagger";
+import { DefuturesService } from "./defutures.service";
+import { TxHashPayload } from "../../../common/payload/txHash.payload";
 
-@Controller('defutures')
-export class DefuturesController {}
+@Controller("v2/defutures")
+export class DefuturesController {
+  constructor(private readonly defuturesService: DefuturesService) {}
+
+  @Post(":chainId/position")
+  @ApiOperation({ summary: "Create a new position" })
+  @ApiCreatedResponse({
+    description: "The position has been successfully created.",
+  })
+  @ApiBadRequestResponse({ description: "Bad request." })
+  @ApiUnauthorizedResponse({ description: "Unauthorized." })
+  @ApiInternalServerErrorResponse({ description: "Internal server error." })
+  async createPosition(
+    @Param("chainId", ParseIntPipe) chainId: number,
+    @Body() txHashPayload: TxHashPayload
+  ) {
+    return await this.defuturesService.createPosition(chainId, txHashPayload);
+  }
+}
--- src/api/V2/defutures/defutures.service.ts
+++ src/api/V2/defutures/defutures.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from "@nestjs/common";
+import { PrismaService } from "src/common/services/prisma.service";
+import { ethers, providers } from "ethers";
 
 @Injectable()
-export class DefuturesService {}
+export class DefuturesService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async validateTxHash(
+    provider: providers.JsonRpcProvider,
+    txHash: string
+  ): Promise<providers.TransactionReceipt> {
+    const receipt = await provider.getTransactionReceipt(txHash);
+    if (!receipt)
+      throw new BadRequestException(
+        `Transaction with hash ${txHash} does not exist.`
+      );
+    return receipt;
+  }
+
+  async createPosition(chainId: number, { txHash }: { txHash: string }) {}
+}
